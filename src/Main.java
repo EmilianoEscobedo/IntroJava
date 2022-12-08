30176746bcdea60a5a2ce9cf362121a122b1eb3a
EmilianoEscobedo <emilianoescobedo9@gmail.com>
2022-12-08 01:23:36 -0300

    Translated to eng

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     public static void main(String[] args){
         System.out.println(suma(10,20,30));
-        Auto miAuto = new Auto();
-        miAuto.sumarPuerta();
-        System.out.println(miAuto.puertas);
+        Car myCar = new Car();
+        myCar.plusDoor();
+        System.out.println(myCar.doors);
     }
 
     public static int suma(int a, int b, int c){
         return a+b+c;
     }
 }
-class Auto{
-    public int puertas = 0;
+class Car{
+    public int doors = 0;
 
-    public void sumarPuerta(){
-        this.puertas++;
+    public void plusDoor(){
+        this.doors++;
     }
 }
 
