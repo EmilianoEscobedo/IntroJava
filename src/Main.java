5f21735db655c386e5914039bda2658d8071a5e3
Emiliano Escobedo <83373185+EmilianoEscobedo@users.noreply.github.com>
2022-12-09 12:24:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class Main {
+/* public class Main {
     public static void main(String[] args){
         System.out.println(suma(10,20,30));
         Car myCar = new Car();
@@ -16,6 +16,59 @@ class Car{
     public void plusDoor(){
         this.doors++;
     }
+} */
+
+////////////////////////////////////////////////////
+
+public class Main {
+    public static void main(String[] args){
+       
+        int numberIf = 10;
+        
+        if (numberIf > 0){
+            System.out.println("The number is positive");
+            }else if(numberIf < 0){
+            System.out.println("The number is negative");
+            } else{
+            System.out.println("The number is zero");
+            };
+        
+        int numberWhile = 0;
+        
+        while (numberWhile < 3){
+            numberWhile++;
+            System.out.println("While executed");
+            };
+        
+        do {
+            numberWhile++;
+            System.out.println("While executed");
+            } while (numberWhile == 0);
+       
+        for (numberFor = 0; numberFor =< 3; numberFor++){
+             System.out.println(numberFor);
+            };
+        
+        String season = "summer";
+        
+        switch (season){
+            case "summer":
+                System.out.println("its summer");
+                break;
+            case "autumn":
+                System.out.println("its summer");
+                break;
+            case "winter":
+                System.out.println("its summer");
+                break;
+            case "spring":
+                System.out.println("its summer");
+                break;
+            default:
+                System.out.println("its not a season");
+           }
+    }
+
 }
 
 
